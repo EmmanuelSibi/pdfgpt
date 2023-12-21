4610a2fd13fd2249d01b0118dbd020f17663915f
EmmanuelSibi <emmanuelsiby2003@gmail.com>
2023-12-21 18:02:54 +0530

    delete file function

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -11,6 +11,7 @@ import { format } from "date-fns";
 
 const DashBoard = () => {
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
+  const {mutate:deleteFile} = trpc.deleteFile.useMutation()
   return (
     <main className="mx-auto max-w-7xl md:p-10">
       <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
@@ -63,6 +64,9 @@ const DashBoard = () => {
                 </div>
 
                 <Button
+                  onClick={() => {
+                    deleteFile({id:file.id})
+                  }}
                  
                   size='sm'
                   className='w-full'
