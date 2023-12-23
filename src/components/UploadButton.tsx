c7a424a131b8b3c04e70f4f760db29d40ad8ac61
EmmanuelSibi <emmanuelsiby2003@gmail.com>
2023-12-24 00:11:22 +0530

    finished fileupload

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -4,28 +4,124 @@ import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
 import Dropzone from "react-dropzone";
+import { Cloud, File, Loader2 } from "lucide-react";
+import { Progress } from "./ui/progress";
+import { useUploadThing } from "@/lib/uploadthing";
+import { useToast } from "./ui/use-toast";
+import { trpc } from "@/app/_trpc/client";
+import { redirect, useRouter } from "next/navigation";
+
 
 const UploadDropZone = () => {
-  return <Dropzone multiple={false} onDrop={(acceptedFile)=>{
-    console.log(acceptedFile)
-  }}>
-    {({ getRootProps, getInputProps }) => (
-      
-        <div {...getRootProps()} className="border h-64  border-dashed border-gray-300 m-4 rounded-lg ">
-          <div className='flex items-center justify-center h-full w-full'>
-          <label
-              htmlFor='dropzone-file'
-              className='flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'>
-              <div className='flex flex-col items-center justify-center pt-5 pb-6'>
-                example
+   const router = useRouter()
+  const [isUploading, setIsUploading] = useState<boolean>(false);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+  const {startUpload} = useUploadThing("pdfUploader");
+  const {toast} =useToast()
+  const {mutate : startPolling} = trpc.getFile.useMutation({
+    onSuccess:(file)=>{
+   
+    router.push(`/dashboard/${file.id}`)
+    
+    },
+    retry:true,
+    retryDelay:500
+  })
+  // function for determinate progress bar
+  const startSimulateProgress = () => {
+    setUploadProgress(0);
+    const interval = setInterval(() => {
+      setUploadProgress((prev) => {
+        if (prev >= 96) {
+          clearInterval(interval);
+          return prev;
+        }
+        return prev + 1;
+      });
+    },600);
+    return interval;
+  };
+
+  return (
+    <Dropzone
+      multiple={false}
+      onDrop={async (acceptedFile) => {
+        setIsUploading(true);
+        const progressInterval = startSimulateProgress();
+
+        //handling file upload
+        const res =  await startUpload(acceptedFile);
+        if(!res){
+          toast({
+            title:"Something went wrong",
+            description:"Please try again later",
+            variant:"destructive"
+          })
+        }
+        const [fileresponse] = res ?? []
+        const key = fileresponse?.key
+        if(!key){
+          toast({
+            title:"Something went wrong",
+            description:"Please try again later",
+            variant:"destructive"
+          })
+        }
+        await new Promise((resolve) => setTimeout(resolve, 20000));
+
+
+        clearInterval(progressInterval);
+        setUploadProgress(100);
+        startPolling({key})
+        
+      }}
+    >
+      {({ getRootProps, getInputProps, acceptedFiles }) => (
+        <div
+          {...getRootProps()}
+          className="border h-64  border-dashed border-gray-300 m-4 rounded-lg "
+        >
+          <div className="flex items-center justify-center h-full w-full">
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                <p className="mb-2 text-sm text-zinc-700">
+                  <span className="font-semibold">Click to upload</span> {` `}{" "}
+                  or drag and drop
+                </p>
+                <p className="text-xs text-zinc-500">PDF files (max 4MB)</p>
+              </div>
+              {acceptedFiles && acceptedFiles[0] ? (
+                <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
+                  <div className="px-3 py-2 h-full grid place-items-center">
+                    <File className="h-4 w-4 text-blue-500" />
+                  </div>
+                  <div className="px-3 py-2 h-full text-sm truncate">
+                    {acceptedFiles[0].name}
+                  </div>
                 </div>
-                </label>
+              ) : null}
+              {isUploading ? (
+                <div className="w-full mt-4 max-w-sm mx-auto">
+                  <Progress value={uploadProgress} className="w-full h-1 bg-zinc-200" />
+                  {uploadProgress === 100 ? (
+                    <div className='flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2'>
+                      <Loader2 className='h-3 w-3 animate-spin' />
+                      Redirecting...
+                    </div>
+                  ) : null}
+                </div>
+              ) : null}
+              <input {...getInputProps()} type="file" id='dropzone-file' className="hidden" />
+            </label>
           </div>
         </div>
-    
-    )}
-
-  </Dropzone>
+      )}
+    </Dropzone>
+  );
 };
 
 const UploadButton = () => {
